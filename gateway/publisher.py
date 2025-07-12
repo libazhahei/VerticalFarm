d116c1ebf2d1da1a24527dc66d33cd2ce543bd87
libazhahei <li77534479@gmail.com>
2025-07-12 13:34:10 +1000

    Adding CICD workflow

--- gateway/publisher.py
+++ gateway/publisher.py
@@ -1,16 +1,17 @@
 import asyncio
+from collections.abc import Awaitable, Callable
 from dataclasses import dataclass
-from typing import Awaitable, Callable, Dict
-from paho.mqtt.client import Client as MQTTClient
-from paho.mqtt.client import MQTT_ERR_SUCCESS
+
+from paho.mqtt.client import MQTT_ERR_SUCCESS, Client as MQTTClient
 
 from gateway.msg import ControlMsg, MessageType
-from .constants import PUBLISH_ERR_MAX_RETRIES, PUBLISH_CTRL_QOS, PUBLISH_RESENT_MAX_RETRIES, PUBLISH_TIMEOUT_SECONDS
+
+from .constants import PUBLISH_CTRL_QOS, PUBLISH_ERR_MAX_RETRIES, PUBLISH_RESENT_MAX_RETRIES, PUBLISH_TIMEOUT_SECONDS
+
 
 @dataclass
 class QueueMessage:
-    """
-    QueueMessage is a data class that represents a message to be published to a queue.
+    """QueueMessage is a data class that represents a message to be published to a queue.
 
     Attributes:
         topic (str): The topic to which the message belongs.
@@ -21,35 +22,41 @@ class QueueMessage:
     Methods:
         from_dict(cls, data: Dict) -> QueueMessage:
             Creates a QueueMessage instance from a dictionary.
-            Args:
+
+    Args:
                 data (Dict): A dictionary containing the keys 'topic', 'payload', 'stop_event', and 'retries_left'.
-            Returns:
+
+    Returns:
                 QueueMessage: An instance of QueueMessage initialized with the provided data.
+
     """
+
     topic: str
     payload: ControlMsg
     stop_event: asyncio.Event
     retries_left: int
 
     @classmethod
-    def from_dict(cls, data: Dict) -> 'QueueMessage':
+    def from_dict(cls, data: dict) -> 'QueueMessage':
+        """Creates a QueueMessage instance from a dictionary."""
         return cls(
             topic=data["topic"],
             payload=data["payload"],
             stop_event=data["stop_event"],
             retries_left=data["retries_left"]
         )
-    
 
-class ControlCommandPublisher: 
-    """
-    A class responsible for publishing control commands to an MQTT broker and managing message acknowledgments.
+
+class ControlCommandPublisher:
+    """A class responsible for publishing control commands to an MQTT broker and managing message acknowledgments.
+
     Attributes:
         mqtt_client (MQTTClient): The MQTT client used for publishing messages.
         max_retries (int): Maximum number of retries for publishing a message.
         timeout (int): Timeout duration (in seconds) for waiting for message acknowledgment.
         msgs (Dict[int, QueueMessage]): A dictionary storing messages being published, keyed by their message IDs.
         is_alive_func (Callable[[int], Awaitable[bool]]): A function to check if a device is alive, given its board ID.
+
     Methods:
         __init__(mqtt_client, is_alive_func, max_retries, timeout):
             Initializes the ControlCommandPublisher instance with the given MQTT client, device status function, 
@@ -67,13 +74,16 @@ class ControlCommandPublisher:
         try_publish(message_id):
             Asynchronously attempts to publish a message by its ID, retrying if necessary and handling acknowledgment 
             or timeout scenarios.
-    """
-    mqtt_client: MQTTClient
-    max_retries: int 
-    timeout: float
-    msgs: Dict[int, QueueMessage]
 
-    def __init__(self, mqtt_client: MQTTClient, is_alive_func: Callable[[int], Awaitable[bool]], max_retries: int = PUBLISH_RESENT_MAX_RETRIES, timeout: float = PUBLISH_TIMEOUT_SECONDS):
+    """
+
+    mqtt_client: MQTTClient
+    max_retries: int
+    timeout: float
+    msgs: dict[int, QueueMessage]
+
+    def __init__(self, mqtt_client: MQTTClient, is_alive_func: Callable[[int], Awaitable[bool]], 
+                 max_retries: int = PUBLISH_RESENT_MAX_RETRIES, timeout: float = PUBLISH_TIMEOUT_SECONDS) -> None:
         self.mqtt_client = mqtt_client
         self.max_retries = max_retries
         self.timeout = timeout
@@ -81,23 +91,26 @@ class ControlCommandPublisher:
         self.is_alive_func = is_alive_func
 
     async def add_msg(self, message: MessageType, topic: str) -> None:
-        """
-        Adds a message to the publishing queue and initiates the publishing process.
+        """Adds a message to the publishing queue and initiates the publishing process.
+
         Args:
             message (MessageType): The message to be published. Must be an instance of `ControlMsg`.
             topic (str): The topic to which the message will be published.
+
         Raises:
             TypeError: If the provided `message` is not an instance of `ControlMsg`.
+
         Notes:
             - The message is stored in the `msgs` dictionary with its unique message ID as the key.
             - A `QueueMessage` object is created from the provided message and topic, 
               along with a stop event and the maximum number of retries.
             - A background task is created to attempt publishing the message using `try_publish`.
+
         """
         if not isinstance(message, ControlMsg):
             raise TypeError("Message must be an instance of ControlMsg")
-        
-        stop_event = asyncio.Event() 
+
+        stop_event = asyncio.Event()
         self.msgs[message.get_message_id()] = QueueMessage.from_dict({
             "topic": topic,
             "payload": message,
@@ -107,8 +120,7 @@ class ControlCommandPublisher:
         asyncio.create_task(self.try_publish(message.get_message_id()))
 
     def acknowledge(self, message_id: int) -> None:
-        """
-        Acknowledges the receipt of a message by its ID and stops any associated waiting process.
+        """Acknowledges the receipt of a message by its ID and stops any associated waiting process.
 
         Args:
             message_id (int): The unique identifier of the message to acknowledge.
@@ -118,6 +130,7 @@ class ControlCommandPublisher:
               message information, signals the stop event to halt any waiting process, and
               removes the message from the dictionary.
             - If the message ID does not exist, no action is taken.
+
         """
         if message_id in self.msgs:
             msg_info = self.msgs[message_id]
@@ -125,14 +138,14 @@ class ControlCommandPublisher:
             del self.msgs[message_id]
 
     def fail_msg(self, message_id: int) -> None:
-        """
-        Handles the failure of a message by stopping its associated event and removing it from the message tracking dictionary.
+        """Handles the failure of a message by stopping its associated event and removing it from the message tracking dictionary.
 
         Args:
             message_id (int): The unique identifier of the message to be marked as failed.
 
         Returns:
             None
+
         """
         if message_id in self.msgs:
             msg_info = self.msgs[message_id]
@@ -141,8 +154,7 @@ class ControlCommandPublisher:
 
 
     def safe_publish(self, message: MessageType, topic: str) -> bool:
-        """
-        Safely publishes a message to a specified MQTT topic with retry logic.
+        """Safely publishes a message to a specified MQTT topic with retry logic.
 
         Args:
             message (MessageType): The message object to be published. It must have a `to_json()` method 
@@ -162,6 +174,7 @@ class ControlCommandPublisher:
               the function logs the error and retries.
             - Exceptions during publishing are caught and logged, and the function retries until the 
               maximum number of attempts is reached.
+
         """
         for attempt in range(PUBLISH_ERR_MAX_RETRIES):
             try:
@@ -175,8 +188,7 @@ class ControlCommandPublisher:
         return False
 
     async def try_publish(self, message_id: int) -> None:
-        """
-        Attempts to publish a message with the given message ID. Retries the operation
+        """Attempts to publish a message with the given message ID. Retries the operation
         if the message fails to publish or is not acknowledged within the timeout period.
 
         Args:
@@ -197,6 +209,7 @@ class ControlCommandPublisher:
             - Waits for an acknowledgment signal (`stop_event`) within the specified timeout.
               If the timeout is exceeded, decrements the retry count and retries the operation.
             - If the retry count reaches zero, marks the message as failed and raises a `TimeoutError`.
+
         """
         if message_id not in self.msgs:
             return
@@ -215,7 +228,7 @@ class ControlCommandPublisher:
             try:
                 await asyncio.wait_for(msg_info.stop_event.wait(), timeout=self.timeout)
                 return  # Acknowledged, exit the loop
-            except asyncio.TimeoutError:
+            except TimeoutError:
                 msg_info.retries_left -= 1
                 if msg_info.retries_left == 0:
                     self.fail_msg(message_id)
@@ -224,4 +237,3 @@ class ControlCommandPublisher:
                     print(f"[TIMEOUT] Message {message_id} not acknowledged, retries left: {msg_info.retries_left}")
                     continue
 
-    
--- gateway/subscriber.py
+++ gateway/subscriber.py
@@ -1,79 +1,96 @@
-from abc import ABC, abstractmethod
 import asyncio
+from abc import ABC, abstractmethod
+from collections.abc import Callable
 from datetime import datetime
-from typing import Callable, List, Set, Tuple, Dict, Type
+
 from aiorwlock import RWLock
+
+from .constants import DEVICE_MAX_ID, DEVICE_MIN_ID, SUBSCRIBE_HEARTBEAT_TIMEOUT_SECONDS
 from .msg import HeartbeatMsg, MessageType, StatusMsg
-from .constants import SUBSCRIBE_HEARTBEAT_TIMEOUT_SECONDS
+
 
 class GenericSubscriber(ABC):
     """
     GenericSubscriber is an abstract base class that defines the interface for a subscriber
-    in a message-handling system. Subclasses must implement the following methods:
-    Methods:
-    ---------
+    in a message-handling system. Subclasses must implement the following methods.
+
+    Methods
+    -------
     - handle(msg: MessageType):
         An asynchronous method that processes a message of type MessageType.
         This method must be implemented by subclasses.
     - parse_json(json_str: str) -> MessageType:
         A static method that parses a JSON string and returns an object of type MessageType.
         This method must be implemented by subclasses.
-    Raises:
-    -------
+
+    Raises
+    ------
     - NotImplementedError:
         Raised if the required methods are not implemented by a subclass.
+
     """
+
     @abstractmethod
-    async def handle(self, msg: MessageType):
+    async def handle(self, msg: MessageType) -> None:
+        """Parses a JSON string into a MessageType object."""
         raise NotImplementedError("Subclasses must implement the handle method.")
-    
+
     @staticmethod
     @abstractmethod
     def parse_json(json_str: str) -> MessageType:
+        """Parses a JSON string into a MessageType object."""
         raise NotImplementedError("Subclasses must implement the parse_json method.")
 
-class HeartbeatSubscriber(GenericSubscriber): 
-    """
-    A subscriber class responsible for handling heartbeat messages from devices
+class HeartbeatSubscriber(GenericSubscriber):
+    """A subscriber class responsible for handling heartbeat messages from devices
     and maintaining their alive status.
+
     Attributes:
         alive_devices (List[Tuple[int, float]]): A list of tuples representing the status of devices.
             Each tuple contains the sequence number and the last timestamp of the heartbeat message.
             Default value is a list of 7 tuples initialized to (-1, -1).
         lock (RWLock): A read-write lock to ensure thread-safe access to `alive_devices`.
+
     Methods:
         __init__():
             Initializes the `alive_devices` list with default values.
         async handle(msg: HeartbeatMsg):
             Handles incoming heartbeat messages and updates the status of the corresponding device.
-            Args:
+
+    Args:
                 msg (HeartbeatMsg): The heartbeat message containing device information.
         async is_alive(board_id: int) -> bool:
             Checks if a device is alive based on its last heartbeat timestamp.
-            Args:
+
+    Args:
                 board_id (int): The ID of the device to check.
-            Returns:
+
+    Returns:
                 bool: True if the device is alive, False otherwise.
         async get_alive_devices() -> List[int]:
             Retrieves a list of IDs of devices that are currently alive.
-            Returns:
+
+    Returns:
                 List[int]: A list of alive device IDs.
         parse_json(json_str: str) -> HeartbeatMsg:
             Parses a JSON string into a `HeartbeatMsg` object.
-            Args:
+
+    Args:
                 json_str (str): The JSON string representing a heartbeat message.
-            Returns:
+
+    Returns:
                 HeartbeatMsg: The parsed heartbeat message object.
+
     """
-    alive_devices: List[Tuple[int, float]] = []  # List of tuples (board_id, seq_no)
+
+    alive_devices: list[tuple[int, float]]  # List of tuples (board_id, seq_no)
     lock: RWLock = RWLock()
 
     def __init__(self):
-        self.alive_devices = [(-1, -1) for _ in range(7)]  
+        self.alive_devices = [(-1, -1) for _ in range(7)]
 
-    async def handle(self, msg: HeartbeatMsg):
-        """
-        Handles incoming heartbeat messages and updates the status of devices.
+    async def handle(self, msg: HeartbeatMsg)-> None:
+        """Handles incoming heartbeat messages and updates the status of devices.
 
         Args:
             msg (HeartbeatMsg): The heartbeat message containing the board ID and sequence number.
@@ -82,14 +99,14 @@ class HeartbeatSubscriber(GenericSubscriber):
             - Acquires a writer lock to ensure thread-safe access to shared resources.
             - Checks if the device corresponding to the given board ID is marked as inactive (-1).
             - Updates the device's status with the sequence number and the current timestamp.
+
         """
         async with self.lock.writer_lock:
             if self.alive_devices[msg.board_id][0] == -1:
                 self.alive_devices[msg.board_id] = (msg.seq_no, datetime.now().timestamp())
 
     async def is_alive(self, board_id: int) -> bool:
-        """
-        Check if a device with the given board ID is alive based on its last heartbeat timestamp.
+        """Check if a device with the given board ID is alive based on its last heartbeat timestamp.
 
         Args:
             board_id (int): The ID of the device board to check. Must be between 0 and 6 inclusive.
@@ -104,12 +121,13 @@ class HeartbeatSubscriber(GenericSubscriber):
               to update the status if the device is stopped.
             - The `SUBSCRIBE_HEARTBEAT_TIMEOUT_SECONDS` constant defines the timeout period 
               for considering a device as alive.
+
         """
         stoped_devices = -1
-        if 0 <= board_id and board_id <= 6:
+        if DEVICE_MIN_ID <= board_id <= DEVICE_MAX_ID:
             async with self.lock.reader_lock:
                 device_status = self.alive_devices[board_id]
-            if device_status != -1: 
+            if device_status != -1:
                 current_time = datetime.now().timestamp()
                 _, last_timestamp = device_status
                 if current_time - last_timestamp < SUBSCRIBE_HEARTBEAT_TIMEOUT_SECONDS:
@@ -120,10 +138,9 @@ class HeartbeatSubscriber(GenericSubscriber):
             async with self.lock.writer_lock:
                 self.alive_devices[stoped_devices] = (-1, -1)
         return False
-    
-    async def get_alive_devices(self) -> List[int]:
-        """
-        Retrieve a list of IDs for devices that are currently alive.
+
+    async def get_alive_devices(self) -> list[int]:
+        """Retrieve a list of IDs for devices that are currently alive.
 
         This asynchronous method acquires a reader lock to ensure thread-safe
         access to the `alive_devices` attribute. It iterates through the list
@@ -133,22 +150,23 @@ class HeartbeatSubscriber(GenericSubscriber):
 
         Returns:
             List[int]: A list of integers representing the IDs of alive devices.
+
         """
         alive_devices = []
         async with self.lock.reader_lock:
-            for board_id, (seq_no, timestamp) in enumerate(self.alive_devices):
+            for board_id, (seq_no, _) in enumerate(self.alive_devices):
                 if seq_no != -1:
                     alive_devices.append(board_id)
         return alive_devices
-    
+
     @staticmethod
     def parse_json(json_str: str) -> HeartbeatMsg:
+        """Parse a JSON string into a HeartbeatMsg object."""
         internal_msg = HeartbeatMsg.from_json(json_str)
         return internal_msg
-    
+
 class CommandResponseSubscriber(GenericSubscriber):
-    """
-    A subscriber class responsible for handling and acknowledging status messages.
+    """A subscriber class responsible for handling and acknowledging status messages.
 
     Attributes:
         acknowledge_func (Callable[[int], None]): A function to acknowledge a message 
@@ -164,23 +182,27 @@ class CommandResponseSubscriber(GenericSubscriber):
 
     Raises:
         TypeError: If the provided message is not an instance of StatusMsg.
+
     """
+
     def __init__(self, acknowledge_func: Callable[[int], None]):
         self.acknowledge_func = acknowledge_func
 
-    async def handle(self, msg: StatusMsg):
+    async def handle(self, msg: StatusMsg) -> None:
+        """Handles a status message by acknowledging it based on the board ID."""
         if not isinstance(msg, StatusMsg):
             raise TypeError("Message must be an instance of StatusMsg")
         self.acknowledge_func(msg.board_id)
 
     @staticmethod
     def parse_json(json_str: str) -> StatusMsg:
+        """Parse a JSON string into a HeartbeatMsg object."""
         internal_msg = StatusMsg.from_json(json_str)
         return internal_msg
 
-class MessageDispatcher: 
-    """
-    A class responsible for dispatching messages to registered handlers asynchronously.
+class MessageDispatcher:
+    """A class responsible for dispatching messages to registered handlers asynchronously.
+
     Attributes:
         subscribers (Dict[Type[MessageType], List[Callable]]): 
             A dictionary mapping message types to lists of subscriber handler functions.
@@ -192,6 +214,7 @@ class MessageDispatcher:
             The asyncio task running the dispatch loop, or None if not running.
         processing_task (Set[asyncio.Task]): 
             A set of asyncio tasks currently processing messages.
+
     Methods:
         register_handler(msg_type: Type[MessageType], subscriber_handle: Callable) -> None:
             Registers a handler function for a specific message type.
@@ -205,18 +228,19 @@ class MessageDispatcher:
             Stops the dispatcher, ensuring all pending messages are processed.
         async put_message(msg: MessageType) -> None:
             Adds a message to the queue for dispatching. Requires the dispatcher to be running.
+
     """
+
     def __init__(self) -> None:
-        self.subscribers : Dict[Type[MessageType], List[Callable]] = {}
+        self.subscribers : dict[type[MessageType], list[Callable]] = {}
         self.message_queue: asyncio.Queue = asyncio.Queue()
         self.running: bool = False
         self.dispatch_task: asyncio.Task | None = None
-        self.processing_task: Set[asyncio.Task]  = set() 
-        
-    
-    def register_handler(self, msg_type: Type[MessageType], subscriber_handle: Callable) -> None:
-        """
-        Registers a handler for a specific message type.
+        self.processing_task: set[asyncio.Task]  = set()
+
+
+    def register_handler(self, msg_type: type[MessageType], subscriber_handle: Callable) -> None:
+        """Registers a handler for a specific message type.
 
         Args:
             msg_type (Type[MessageType]): The type of message to associate with the handler.
@@ -224,14 +248,14 @@ class MessageDispatcher:
 
         Returns:
             None
+
         """
         if msg_type not in self.subscribers:
             self.subscribers[msg_type] = []
         self.subscribers[msg_type].append(subscriber_handle)
-    
+
     async def dispatch(self, msg: MessageType) -> None:
-        """
-        Dispatches a message to the appropriate handlers based on its type.
+        """Dispatches a message to the appropriate handlers based on its type.
 
         This method retrieves the handlers registered for the type of the given 
         message and asynchronously executes them. If no handlers are registered 
@@ -251,10 +275,11 @@ class MessageDispatcher:
         Note:
             Exceptions raised by handlers are not propagated; instead, they are 
             collected and returned as part of `asyncio.gather`.
+
         """
         msg_type = type(msg)
         handlers = self.subscribers.get(msg_type, None)
-        if handlers is None: 
+        if handlers is None:
             print(f"No handler registered for message type {msg_type}.")
             return
         tasks = []
@@ -266,9 +291,8 @@ class MessageDispatcher:
 
         await asyncio.gather(*tasks, return_exceptions=True)
 
-    async def dispatch_loop(self):
-        """
-        Asynchronous method that continuously processes messages from a queue and dispatches them
+    async def dispatch_loop(self) -> None:
+        """Asynchronous method that continuously processes messages from a queue and dispatches them
         to appropriate subscribers based on their type.
 
         This method runs in a loop while the `running` attribute is True. It retrieves messages
@@ -290,6 +314,7 @@ class MessageDispatcher:
 
         Raises:
             asyncio.CancelledError: If the loop is cancelled.
+
         """
         while self.running:
             try:
@@ -308,9 +333,8 @@ class MessageDispatcher:
             finally:
                 print(f"Message {msg} processed.")
 
-    async def start(self):
-        """
-        Starts the MessageDispatcher if it is not already running.
+    async def start(self) -> None:
+        """Starts the MessageDispatcher if it is not already running.
 
         This method initializes the dispatcher by setting the `running` flag to True
         and creating an asynchronous task for the dispatch loop. If the dispatcher
@@ -318,6 +342,7 @@ class MessageDispatcher:
 
         Raises:
             RuntimeError: If the dispatcher is already running.
+
         """
         if self.running:
             raise RuntimeError("Dispatcher is already running.")
@@ -325,9 +350,8 @@ class MessageDispatcher:
         self.dispatch_task = asyncio.create_task(self.dispatch_loop())
         print("MessageDispatcher started.")
 
-    async def stop(self):
-        """
-        Stops the MessageDispatcher gracefully.
+    async def stop(self) -> None:
+        """Stops the MessageDispatcher gracefully.
 
         This method stops the dispatcher if it is currently running. It cancels the dispatch task,
         waits for all messages in the queue to be processed, and ensures that any processing tasks
@@ -341,6 +365,7 @@ class MessageDispatcher:
             - Cancels the `dispatch_task` and waits for its completion.
             - Waits for all messages in the `message_queue` to be processed.
             - Gathers and handles exceptions for any tasks in `processing_task`.
+
         """
         if not self.running:
             raise RuntimeError("Dispatcher is not running.")
@@ -358,6 +383,16 @@ class MessageDispatcher:
         print("MessageDispatcher stopped.")
 
     async def put_message(self, msg: MessageType) -> None:
+        """
+        Asynchronously adds a message to the message queue.
+
+        Args:
+            msg (MessageType): The message to be added to the queue.
+
+        Raises:
+            RuntimeError: If the dispatcher is not running. Ensure `start()` is called before using this method.
+
+        """
         if not self.running:
             raise RuntimeError("Dispatcher is not running. Call start() before putting messages.")
         await self.message_queue.put(msg)
