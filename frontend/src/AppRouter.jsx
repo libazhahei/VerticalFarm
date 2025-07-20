a9db9bcd80ac6f9376f1e83b8b6784a8071c3df4
RILEYLIUUU <riley.xyliu@gmail.com>
2025-07-20 21:43:24 +1000

    latest version of frontend

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './page/Dashboard';
+import ReportPage from './page/Report';
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard/>} />
+      <Route path="/report" element={<ReportPage/>} />
     </Routes>
   );
 };
