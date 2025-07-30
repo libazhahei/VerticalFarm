513bbb118071518c7906764d1c7fe3b30d010b0e
哈基米那没路多，阿西嘎哈亚库马路 <zixi.qi@bngu.io>
2025-07-30 15:27:06 +1000

    problem is set

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 from data.config import init_schema
 from gateway import ControlMsg, MQTTServiceContext
 from gateway.service import BLEServiceContext
-
-
+from time import sleep 
+from tortoise import Tortoise
 async def main() -> None:
     """Main function to demonstrate the usage of MQTTServiceContext and BLEServiceContext."""
     mqtt_service_context = MQTTServiceContext(broker_host="localhost", broker_port=5001, client_id="test_client")
@@ -23,12 +23,15 @@ async def main() -> None:
 
         ctrl_msg = ControlMsg(board_id=1)
         await mqtt_service_context.publish_control_command(ctrl_msg)
-        print("BLE connected devices:", ble_service_context.connected_devices())
+        for _ in range(1):
+            print("BLE connected devices:", ble_service_context.connected_devices())
+            sleep(10)
         data = await ble_service_context.fetch_data(since=datetime.now() - timedelta(days=1), board_ids=[1])
         print(f"Fetched data: {data}")
     finally:
         await mqtt_service_context.stop()
         await ble_service_context.stop()
+        await Tortoise.close_connections()
 
 
 if __name__ == "__main__":
