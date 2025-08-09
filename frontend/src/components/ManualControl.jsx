a65d2d1f676b6b467b623484c4b52f3dad9e884f
RILEYLIUUU <riley.xyliu@gmail.com>
2025-08-09 21:47:31 +1000

    add

--- frontend/src/components/ManualControl.jsx
+++ frontend/src/components/ManualControl.jsx
@@ -178,7 +178,7 @@ export default function ManualControl (props) {
             onChange={(e, v) => setLightIntensity(v)}
             valueLabelDisplay="auto"
             min={0}
-            max={2000}
+            max={4000}
             disabled={!isManual}
           />
         </Box>
